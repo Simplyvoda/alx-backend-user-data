d654326c5a5630a0a7bce0eabc6eae9be8064063
Simplyvoda <efem.vodina@gmail.com>
2024-04-17 00:56:23 +0100

    update: defining routes that dont need auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Authentication module for the API.
 """
+import re
 from flask import request
 from typing import List, TypeVar
 
@@ -17,12 +18,23 @@ class Auth():
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
-        Checks if user is authenticated
+        Checks if path requires authentication
         :param path: path to check
         :param excluded_paths: list of paths to exclude
         :return: True if user is authenticated, False otherwise
         """
-        return False
+        if path is not None and excluded_paths is not None:
+            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if exclusion_path[-1] == '*':
+                    pattern = '{}.*'.format(exclusion_path[0:-1])
+                elif exclusion_path[-1] == '/':
+                    pattern = '{}/*'.format(exclusion_path[0:-1])
+                else:
+                    pattern = '{}/*'.format(exclusion_path)
+                if re.match(pattern, path):
+                    return False
+        return True
     
 
     def authorization_header(self, request=None) -> str:
