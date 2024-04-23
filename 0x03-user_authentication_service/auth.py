6b68093bf56b36bbc2d80fe3654cfae624592bff
Simplyvoda <efem.vodina@gmail.com>
2024-04-23 21:51:35 +0000

    update: added auth.py file

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module for Authentication methods
+"""
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """
+    Returns salted hash of input password
+    """
+    hashed_pwd = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+    return hashed_pwd
