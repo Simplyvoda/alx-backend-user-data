93ccb46c1bd6fb92e5d2e43a174c7dd7a6af96d1
Simplyvoda <efem.vodina@gmail.com>
2024-04-23 22:52:13 +0000

    update: register user on auth file

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -13,9 +13,27 @@ def _hash_password(password: str) -> bytes:
     hashed_pwd = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
     return hashed_pwd
 
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
 
     def __init__(self):
+        """Initialises the DB
+        """
         self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Registers a new user in the DB
+        if not already registered
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                raise ValueError(f"User {email} already exists")
+            else:
+                self._db.add_user(email, _hash_password(password))
+                return user
+        except Exception:
+            raise RuntimeError(f"Failed to register user")
