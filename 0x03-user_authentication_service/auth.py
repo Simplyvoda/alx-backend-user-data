01b5a65f1ac95f8b9bf38f74d837c0c3e0715803
Simplyvoda <efem.vodina@gmail.com>
2024-04-27 21:01:14 +0000

    update: destroy session task

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -88,10 +88,5 @@ class Auth:
         """
         if user_id is None:
             return None
-        try:
-            user = self._db.find_user_by(user_id=user_id)
-            if user:
-                self._db.update_user(user.id, session_id=None)
-        except NoResultFound:
-            return None
 
+        self._db.update_user(user_id, session_id=None)
